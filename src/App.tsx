dad6019590b9edb7a6075d1674bc4c8007552433
Mohammad <mohammadpoorjamal@gmail.com>
2024-10-10 15:27:45 +0330

    v-0.0.11: remove show selected count from autocomplete

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
 import {useForm} from "react-hook-form";
 import "./index.css"
-import {TFormSchema} from "./lib/components/input-list/type";
+import {IFormOption, TFormSchema} from "./lib/components/input-list/type";
 import {Form} from "./lib";
+import Box from "@mui/material/Box";
+import {Button} from "@mui/material";
 
 function App() {
+
+  const handleSubmit = (values: any) => {
+    console.log('values', values)
+  }
+
+  const options: IFormOption[] = [
+    {
+      label: "a",
+      value: "a"
+    },
+    {
+      label: "b",
+      value: "b"
+    },
+  ]
 
   const form = useForm()
   const schema: TFormSchema = [
     {
       name: "test",
       label: "test",
-      type: "number",
+      type: "auto-complete",
+      options
     }
   ]
 
   return (
-    <>
+    <Box component={'form'} onSubmit={form.handleSubmit(handleSubmit)}>
       <Form schema={schema} form={form} />
-    </>
+
+      <Button type={'submit'}>submit</Button>
+    </Box>
   )
 }
 
